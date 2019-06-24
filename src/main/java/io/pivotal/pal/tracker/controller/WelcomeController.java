8ee902b6f45020dd1a985469c79c8e399002f820
guchun <460428677@qq.com>
2019-06-24 14:45:45 +0800

    update

--- src/main/java/io/pivotal/pal/tracker/controller/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/controller/WelcomeController.java
@@ -1,13 +1,17 @@
 package io.pivotal.pal.tracker.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
